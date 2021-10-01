6ae8131a3df9ad446030689221402eb0a3f50894
Freddy Canido <freddy.canido12@gmail.com>
2021-10-01 15:31:52 -0400

    looping and splitting added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import com.company.cardGame.actor.Dealer;
 import com.company.cardGame.actor.Player;
 import com.company.cardGame.blackJack.Actor;
 import com.company.cardGame.blackJack.Hand;
+import com.company.cardGame.blackJack.Table;
 import com.company.cardGame.deck.Deck;
 import com.company.cardGame.deck.RiggedDeck;
 import com.company.cardGame.deck.StandardDeck;
@@ -14,26 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Deck deck = new StandardDeck();
-        deck.shuffle();
-        Actor dealer = new Player(Console.getString("Player name?", true));
-        Hand myHand = new Hand(dealer);
-        myHand.addCard(deck.draw());
-        myHand.addCard(deck.draw());
-
-        switch (myHand.getAction()){
-            case 1:
-                myHand.addCard(deck.draw());
-                break;
-            case 2:
-                System.exit(1);
-                break;
-            case 3:
-        }
-        while (myHand.getAction() == Actor.HIT) {
-            myHand.addCard(deck.draw());
-            System.out.println("HIT");
-        }
-        System.out.println("Done");
+        Table table = new Table();
+        table.playGame();
     }
 }
